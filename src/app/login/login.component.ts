ddd7e95a9ba608c6e17680ca72ada76c84a5325d
Wilson Santos <wilsonsantosnet@gmail.com>
2020-04-23 00:28:20 -0300

    add type login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,39 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../common/services/auth.service'
 import { GlobalService } from '../global.service';
+import { ETypeLogin } from '../common/type-login.enum';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
 
-    typeLogin: string;
-    sub: any;
-    operation: string;
+  typeLogin: ETypeLogin;
+  ETypeLogin: any;
+  sub: any;
+  operation: string;
 
-    constructor(private authService: AuthService, private route: ActivatedRoute) {
-        this.typeLogin = GlobalService.getAuthSettings().TYPE_LOGIN;
-        console.log(this.typeLogin);
+  constructor(private authService: AuthService, private route: ActivatedRoute, private router: Router) {
+    this.typeLogin = GlobalService.getAuthSettings().TYPE_LOGIN;
+    this.ETypeLogin = ETypeLogin;
+
+    console.log(this.typeLogin);
+  }
+
+  ngOnInit() {
+
+    this.sub = this.route.params.subscribe(params => {
+      this.operation = params['operation'];
+    });
+
+
+    if (this.operation == "out") {
+      console.log(this.operation);
+      this.authService.logout();
     }
-
-    ngOnInit() {
-
-        this.sub = this.route.params.subscribe(params => {
-            this.operation = params['operation'];
-        });
-
-
-        if (this.operation == "out") {
-            console.log(this.operation);
-            this.authService.logout();
-        }
-        else {
-            this.authService.loginSso();
-        }
+    else {
+      this.authService.loginSso();
     }
+  }
 
 }
